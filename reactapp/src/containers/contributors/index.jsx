dc10023557a2ae88d07192188fcb08879bda0336
Jared Williams <accounts@jaredwilliams.com.au>
2020-05-16 17:05:50 +0800

    Add build status

--- reactapp/src/containers/contributors/index.jsx
+++ reactapp/src/containers/contributors/index.jsx
@@ -12,17 +12,26 @@ const contributors = [
 ]
 
 export default () => (
-  <ul>
-    {contributors.map(({ name, items }) => (
-      <li key={name}>
-        <strong>{name}</strong>
-        <br />
-        <ul>
-          {items.map(item => (
-            <li key={item}>{item}</li>
-          ))}
-        </ul>
-      </li>
-    ))}
-  </ul>
+  <>
+    <h1>Contributors</h1>
+    <p>People that have helped develop the site.</p>
+    <ul>
+      {contributors.map(({ name, items }) => (
+        <li key={name}>
+          <strong>{name}</strong>
+          <br />
+          <ul>
+            {items.map(item => (
+              <li key={item}>{item}</li>
+            ))}
+          </ul>
+        </li>
+      ))}
+    </ul>
+    <h2>Build Status</h2>
+    <img
+      src="https://api.netlify.com/api/v1/badges/d9ba52e1-13fa-4b88-94cf-a7d6065111a5/deploy-status"
+      alt="Status of build"
+    />
+  </>
 )
