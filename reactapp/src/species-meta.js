e1cb637596f5ea49aaf2fffc4cb7273a174fd698
Jared Williams <accounts@jaredwilliams.com.au>
2020-05-17 12:30:55 +0800

    Re-do all species descs

--- reactapp/src/species-meta.js
+++ reactapp/src/species-meta.js
@@ -11,7 +11,8 @@ import { species } from './tags'
 export default {
   [species.Shiba]: {
     name: 'Shiba Inu',
-    shortDescription: 'Foo',
+    shortDescription:
+      'A cute squishy anthropomorphic shibu inu avatar created by Pikapetey.',
     description: `An anthropomorphic avatar created by [Pikapetey](https://www.patreon.com/pikapetey).
 
 To download the Shiba avatar you must first donate $5 to Pikapetey's [Patreon](https://www.patreon.com/pikapetey) then follow the link to join his Discord. From there you can check the #announcements channel for a link to the Unity asset and Substance Painter project.
@@ -26,37 +27,65 @@ Alternatively you can use the more optimized "Corpse Shiba" that is also availab
   },
   [species.Avali]: {
     name: 'Avali',
-    description: ``,
+    shortDescription: `A small fluffy bird-like creature from a RimWorld mod.`,
+    description: `The Avali are a race of small fluffy nomadic pack hunters with bad temper and specific comfort temperature range.
+    
+They were originally a [RimWorld mod](https://rimworldbase.com/avali-mod/) but have since been made into a VRChat avatar and is very popular amongst players.
+
+Useful links:
+- [The Official Avali Wiki](https://avali.fandom.com/wiki/The_Official_Avali_Wiki)`,
     thumbnailUrl: avaliImageUrl
   },
   [species.BestBoi]: {
     name: 'Best Boi',
-    description: ``,
+    shortDescription: `A very fluffy anthropomorphic creature that is the best of the bois.`,
+    description: `A full description is coming soon. For now you can visit the [Best Boi Discord](https://discord.com/invite/sRrXpyZ) to get the latest models.`,
     thumbnailUrl: bestBoiImageUrl
   },
   [species.sergal]: {
     name: 'Sergal',
-    description: ``,
+    shortDescription: `A tall, shark-like fictional alien species.`,
+    description: `The sergal is a fictional alien species created by [Mick Ono](https://en.wikifur.com/wiki/Trancy_MICK). They have a shark-like head and are nicknamed "cheese wedge" by some.
+
+Unfortunately the base model for the VRChat sergal cannot be found at this time.
+
+Useful links:
+- [Wikifur Sergal page](https://en.wikifur.com/wiki/Sergal)`,
     thumbnailUrl: sergalImageUrl
   },
   [species.rexouium]: {
     name: 'Rexouium',
-    description: ``,
+    shortDescription: `A tall fictional species with the name meaning king (rex) care-taker (ouium).`,
+    description: `A tall fictional species with the name meaning king (rex) care-taker (ouium). Created by [RacoonRezillo](https://www.furaffinity.net/view/36134921). They are the king care-takers of their world. They can climb well and are fast.
+    
+You can purchase the base model from the author [here](https://gumroad.com/l/MYutV).
+
+Useful links:
+- [VRChat World](https://www.vrchat.com/home/launch?worldId=wrld_3a278e64-36c3-4c19-9e2a-f8bac7bbd9c4)`,
     thumbnailUrl: rexouiumImageUrl
   },
   [species.kangaroo]: {
     name: 'Kangaroo',
-    description: ``,
+    shortDescription: `A smooth, curvy anthro kangaroo.`,
+    description: `An anthropomorphic kangaroo avatar created by [Spaghet](https://gumroad.com/spaghet_vr).
+
+Base model available for purchase [here](https://gumroad.com/l/TQmtwW).`,
     thumbnailUrl: kangarooImageUrl
   },
   [species.racoon]: {
-    name: 'Racooon',
-    description: ``,
+    name: 'Racoon',
+    shortDescription: `A big-eyed anthro racoon.`,
+    description: `An anthropomorphic racoon model created by [YellowStumps](https://www.furaffinity.net/user/yellowstumps).
+    
+Useful links:
+- [FurAffinity page](https://www.furaffinity.net/view/32997273)
+- [Download the base model](https://drive.google.com/drive/folders/1gny4og32AwilVQcjhSFa-PsxbYEwbXBd)`,
     thumbnailUrl: racoonImageUrl
   },
   [species.otherSpecies]: {
     name: 'Other Species',
-    description: ``,
+    shortDescription: `Assets that have tags not matching the popular ones.`,
+    description: `Assets that have tags not matching the popular ones.`,
     thumbnailUrl: otherSpeciesImageUrl
   }
 }
